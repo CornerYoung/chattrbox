dda6c6c3259d1191251338e8729f76b7fe28aa31
ygn <947498533@qq.com>
2018-04-02 15:25:33 +0800

    使用path模块，创建自定义模块

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var path = require('path');
 
 var server = http.createServer(function(req,res){
   console.log('Responding to a request.');
@@ -10,7 +11,8 @@ var server = http.createServer(function(req,res){
     fileName = url.substring(1);  //去掉 url 首字符，即 ‘/’
   };
   console.log(fileName);
-  fs.readFile('app/index.html',function(err,data){
+  var filePath = path.resolve(__dirname,'app',fileName);
+  fs.readFile(filePath,function(err,data){
     res.end(data);  //回调函数中，使用 res.end 返回文件内容，而不再是返回 HTML 文本
   });
 });
