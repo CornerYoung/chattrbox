28714b775b98b195e838ed441077c1fd1efeaced
ygn <947498533@qq.com>
2018-04-02 14:23:25 +0800

    用fs模块读取文件，处理请求url

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 var http = require('http');
+var fs = require('fs');
 
 var server = http.createServer(function(req,res){
   console.log('Responding to a request.');
-  res.end('<h1>Hello, World</h1>');
+  var url = req.url;
+
+  var fileName = 'index.html';
+  if(url.length > 1){
+    fileName = url.substring(1);  //去掉 url 首字符，即 ‘/’
+  };
+  console.log(fileName);
+  fs.readFile('app/index.html',function(err,data){
+    res.end(data);  //回调函数中，使用 res.end 返回文件内容，而不再是返回 HTML 文本
+  });
 });
 server.listen(3000);  //端口绑定
